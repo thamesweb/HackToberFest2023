f2dfc352b72e6a0a22215dd8b7872c647542cffa
Kanav Phull <phull.kanav@gmail.com>
2021-10-14 14:17:34 +0530

    Added Facial Recognition Project
    New Python Project Added

---
+++ Projects/Python/FaceRecognitionProject/AttendaceProject.py
@@ -0,0 +1,76 @@
+import cv2
+import numpy as np
+import face_recognition
+import os
+from datetime import datetime
+
+path = 'ImagesAttendance'
+images = []
+classNames = []
+myList = os.listdir(path)
+print(myList)
+for cls in myList:
+    curImg = cv2.imread(f'{path}/{cls}')
+    # we could have used the load image file method here, but we used imread one!
+    images.append(curImg)
+    classNames.append(os.path.splitext(cls)[0])
+print(classNames)
+
+
+def find_encoding(images):
+    encodeList = []
+    for img in images:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+    return encodeList
+
+
+def mark_attendance(name):
+    with open('Attendance.csv', 'r+') as f:
+        myDataList = f.readlines()
+        nameList = []
+
+        for line in myDataList:
+            entry = line.split(',')
+            nameList.append(entry[0])
+
+        if name not in nameList:
+            now = datetime.now()
+            dtString = now.strftime('%H:%M:%S')
+            f.writelines(f'\n{name}, {dtString}')
+
+
+encodeListKnown = find_encoding(images)
+print('Encoding Complete!')
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    success, img = cap.read()
+    imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
+    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+
+    facesCurFrame = face_recognition.face_locations(imgS)
+    encodesCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
+
+    for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
+        matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+        faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+        print(faceDis)
+        matchIndex = np.argmin(faceDis)
+
+        if faceDis[matchIndex] < 0.5:
+            name = classNames[matchIndex]
+            mark_attendance(name)
+        else:
+            name = 'Unknown'
+        # print(name)
+        y1, x2, y2, x1 = faceLoc
+        y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+        cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
+        cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 2)
+
+    cv2.imshow('WebCam', img)
+    cv2.waitKey(1)
